b0245486194be10189ec7035e2798566a7199c0b
niuniu315 <454378439@qq.com>
2022-09-13 08:31:06 +0800

    完成chart3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,9 @@
           <Chart1/>
           <Chart2/>
         </section>
-        <section class="bordered section2"></section>
+        <section class="section2">
+          <Chart3/>
+        </section>
         <section class="bordered section3"></section>
         <section class="bordered section4"></section>
         <section class="bordered section5"></section>
@@ -23,10 +25,11 @@
 import '../assets/home.scss';
 import Chart1 from './charts/Chart1.vue';
 import Chart2 from './charts/Chart2.vue';
+import Chart3 from './charts/Chart3.vue';
 
 export default {
   name: 'Home',
-  components: {Chart2, Chart1},
+  components: {Chart3, Chart2, Chart1},
   setup() {
     const year = new Date().getFullYear();
     return {year};
